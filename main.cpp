885ef4fd7d95f599e41a7e44b337838ea7ccfebe
dave <davidvalle421@gmail.com>
2022-05-13 20:14:40 -0400

    added enqueue, dequeue, and queueSize

--- main.cpp
+++ main.cpp
@@ -15,9 +15,43 @@ class Queue{
     public:
         Customer *head;
         Queue(){
+            head = NULL;
 
         }    
         void enqueue(int arriveTime, int orderTime){
+            Customer *temp = new Customer();
+            //code for enqueue, add this customer to Queue
+            cout << "Just added a new customer to line: "<< temp->inTime << " and order: " << temp->orderTime << endl;
+
+            if(head == NULL){
+                head = temp;
+            }
+            else{
+                Customer *another;
+                another = head; //start at the beginning
+                while(another->next != NULL){
+                    another = another->next;
+                }
+                //when we are at the end, we want to make this new chunk(temp0 the last one
+                another->next = temp;
+                temp->next = NULL;
+            }
+        }
+
+    void dequeue(){
+        if(head == NULL){
+            cout << "Empty queue, nothing to delete" << endl;
+        }
+        else{
+            Customer *temp;
+            temp = head; //grab the addr of the first custpmer -- we want to delete this
+            head = head->next; //head moves to the next customer
+            cout << "At this time: " << storeClock << " we have a departing customer" << endl;
+            delete temp;
+        }
+
+        int queSize(){
+            return 1;
 
         }
 };
